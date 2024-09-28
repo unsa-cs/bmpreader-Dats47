9bdf9caaa39d6475c88e12076ab7409045869919
Gerardo <108853876+Gerardo41@users.noreply.github.com>
2024-09-27 20:49:20 -0500

    Update main.c

--- main.c
+++ main.c
@@ -17,7 +17,11 @@ void convertGrayScale(BMPImage* image) {
     unsigned int r=image->data[i*3+2]:
     unsigned int g=image->data[i*3+1];
     unsigned int b=image->data[i*3];
-    unsigned promedio = r + g + b;
+    unsigned gray = (r + g + b)/3; // promedio
+    
+    image->data[i*3+2] = gray; // R
+    image->data[i*3+1] = gray; // G
+    image->data[i*3] = gray; // B
   }
 }
 
